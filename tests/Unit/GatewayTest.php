4dafcf5d8e69f605a3c31b854f353d983b997bfd
Htet Aung Khant <htetaungkhant@securelinkmm.com>
2023-07-01 19:08:04 +0630

    mockery test

--- tests/Unit/GatewayTest.php
+++ tests/Unit/GatewayTest.php
@@ -2,22 +2,32 @@
 
 use Hak\GatewayMyanmar\Facades\Gateway;
 
-it('can send a request to payment gateway response back with array of webRedirectUrl, paymentToken and respCode and respDesc', function(){
-    $gateway = Gateway::process([
+beforeEach(function(){
+    $this->paraemters = [
         'amount' => 10000,
         'description' => 'test payment test',
         'invoice_no' => time(),
         'name' => 'Htet Aung Khant',
         'email' => 'htet@gmail.com'
-    ]);
+    ];
 
-    expect($gateway)->toBeArray();
+    $this->inquiryParams = [
+        'invoice_no' => 1684666236
+    ];
+});
+
+it('can send a request to payment gateway response back with array of webRedirectUrl, paymentToken and respCode and respDesc', function(){
+    $gateway = Mockery::mock(Gateway::process($this->paraemters));
+
+    $mockGateway = $gateway->shouldReceive($gateway)->andReturn(['webPaymentUrl', 'paymentToken', 'respCode', 'respDesc']);
+
+    expect($mockGateway)->toBeObject();
 });
 
 it('can request a payment gateway to inquiry and response back with array of payment inquiry data', function(){
-    $gateway = Gateway::complete([
-        'invoice_no' => 1684666236,
-    ]);
+    $gateway = Mockery::mock(Gateway::complete($this->inquiryParams));
 
-    expect($gateway)->toBeArray();
+    $mockGateway = $gateway->shouldReceive($gateway)->andReturn(['cardNo', 'respCode', 'respDesc']);
+
+    expect($mockGateway)->toBeObject();
 });
